2a38d67ef7905a9234b992cd9fdb61ea106a2879
Melacarol <meluscarol.2003@gmail.com>
2023-04-02 20:26:54 -0500

    V2

---
+++ FLUJO DE CONTROL/EJERCICIO 2.py
@@ -0,0 +1,8 @@
+print("Curso: FUNDAMENTOS DE PYTHON")
+print("Estudiante: Melanie Torres")
+print("Fecha: 30/03/2023\n")
+edad=int(input("ingrese su edad: "))
+if edad>=18:
+    print("mayor de edad")
+else: 
+    print("menor de edad")
---
+++ FLUJO DE CONTROL/EJERCICIO 20.py
@@ -0,0 +1,10 @@
+print("Curso: FUNDAMENTOS DE PYTHON")
+print("Estudiante: Melanie Torres")
+print("Fecha: 02/04/2023\n")
+dias = int(input("Introduce la cantidad de días: "))
+
+horas = dias * 24
+minutos = horas * 60
+segundos = minutos * 60
+
+print(f"{dias} días tienen {horas} horas, {minutos} minutos y {segundos} segundos.")
